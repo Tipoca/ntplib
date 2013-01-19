9efdfb5dae004ed68e29b2e609f0917a80325a3c
Siraaj Khandkar <siraaj@khandkar.net>
2013-01-18 23:17:02 -0500

    Version 0.3.0 from the Cheeseshop.

--- ntplib.py
+++ ntplib.py
@@ -16,87 +16,165 @@
 # with this program; if not, write to the Free Software Foundation, Inc., 59
 # Temple Place, Suite 330, Boston, MA 0.1.2-1307 USA
 ###############################################################################
-
-"""Pyton NTP library.
+"""Python NTP library.
 
 Implementation of client-side NTP (RFC-1305), and useful NTP-related
 functions.
 """
 
 
+import datetime
 import socket
 import struct
 import time
-import datetime
 
    
-# compute delta between system epoch and NTP epoch
-SYSTEM_EPOCH = datetime.date(*time.gmtime(0)[0:3])
-NTP_EPOCH = datetime.date(1900, 1, 1)
-NTP_DELTA = (SYSTEM_EPOCH - NTP_EPOCH).days * 24 * 3600
-
-
 class NTPException(Exception):
     """Exception raised by this module."""
+    pass
 
 
-class NTPPacket(object):
+class NTP:
+    """Helper class defining constants."""
+
+    _SYSTEM_EPOCH = datetime.date(*time.gmtime(0)[0:3])
+    """system epoch"""
+    _NTP_EPOCH = datetime.date(1900, 1, 1)
+    """NTP epoch"""
+    NTP_DELTA = (_SYSTEM_EPOCH - _NTP_EPOCH).days * 24 * 3600
+    """delta between system and NTP time"""
+
+    REF_ID_TABLE = {
+            'DNC': "DNC routing protocol",
+            'NIST': "NIST public modem",
+            'TSP': "TSP time protocol",
+            'DTS': "Digital Time Service",
+            'ATOM': "Atomic clock (calibrated)",
+            'VLF': "VLF radio (OMEGA, etc)",
+            'callsign': "Generic radio",
+            'LORC': "LORAN-C radionavidation",
+            'GOES': "GOES UHF environment satellite",
+            'GPS': "GPS UHF satellite positioning",
+    }
+    """reference identifier table"""
+
+    STRATUM_TABLE = {
+        0: "unspecified",
+        1: "primary reference",
+    }
+    """stratum table"""
+
+    MODE_TABLE = {
+        0: "unspecified",
+        1: "symmetric active",
+        2: "symmetric passive",
+        3: "client",
+        4: "server",
+        5: "broadcast",
+        6: "reserved for NTP control messages",
+        7: "reserved for private use",
+    }
+    """mode table"""
+
+    LEAP_TABLE = {
+        0: "no warning",
+        1: "last minute has 61 seconds",
+        2: "last minute has 59 seconds",
+        3: "alarm condition (clock not synchronized)",
+    }
+    """leap indicator table"""
+
+
+class NTPPacket:
     """NTP packet class.
 
-    This class abstracts the stucture of a NTP packet."""
+    This represents an NTP packet.
+    """
     
-    # packet format to pack/unpack
-    ntp_packet_format = '!B B B b 11I'
-    
+    _PACKET_FORMAT = "!B B B b 11I"
+    """packet format to pack/unpack"""
+
     def __init__(self, version=2, mode=3, tx_timestamp=0):
+        """Constructor.
+
+        Parameters:
+        version      -- NTP version
+        mode         -- packet mode (client, server)
+        tx_timestamp -- packet transmit timestamp
+        """
         self.leap = 0
+        """leap second indicator"""
         self.version = version
+        """version"""
         self.mode = mode
+        """mode"""
         self.stratum = 0
+        """stratum"""
         self.poll = 0
+        """poll interval"""
         self.precision = 0
+        """precision"""
         self.root_delay = 0
+        """root delay"""
         self.root_dispersion = 0
+        """root dispersion"""
         self.ref_id = 0
+        """reference clock identifier"""
         self.ref_timestamp = 0
+        """reference timestamp"""
         self.orig_timestamp = 0
+        """originate timestamp"""
         self.recv_timestamp = 0
+        """receive timestamp"""
         self.tx_timestamp = tx_timestamp
+        """tansmit timestamp"""
         
     def to_data(self):
-        """convert a NTPPacket to a NTP packet that can be sent over network
-        raise a NTPException in case of invalid field"""
+        """Convert this NTPPacket to a buffer that can be sent over a socket.
+
+        Returns:
+        buffer representing this packet
+
+        Raises:
+        NTPException -- in case of invalid field
+        """
         try:
-            packed = struct.pack(NTPPacket.ntp_packet_format,
+            packed = struct.pack(NTPPacket._PACKET_FORMAT,
                 (self.leap << 6 | self.version << 3 | self.mode),
                 self.stratum,
                 self.poll,
                 self.precision,
-                to_int(self.root_delay) << 16 | to_frac(self.root_delay, 16),
-                to_int(self.root_dispersion) << 16 |
-                    to_frac(self.root_dispersion, 16),
+                _to_int(self.root_delay) << 16 | _to_frac(self.root_delay, 16),
+                _to_int(self.root_dispersion) << 16 |
+                _to_frac(self.root_dispersion, 16),
                 self.ref_id,
-                to_int(self.ref_timestamp),
-                to_frac(self.ref_timestamp),
-                to_int(self.orig_timestamp),
-                to_frac(self.orig_timestamp),
-                to_int(self.recv_timestamp),
-                to_frac(self.recv_timestamp),
-                to_int(self.tx_timestamp),
-                to_frac(self.tx_timestamp))
+                _to_int(self.ref_timestamp),
+                _to_frac(self.ref_timestamp),
+                _to_int(self.orig_timestamp),
+                _to_frac(self.orig_timestamp),
+                _to_int(self.recv_timestamp),
+                _to_frac(self.recv_timestamp),
+                _to_int(self.tx_timestamp),
+                _to_frac(self.tx_timestamp))
         except struct.error:
-            raise NTPException('Invalid NTP packet fields')
-
+            raise NTPException("Invalid NTP packet fields.")
         return packed
 
     def from_data(self, data):
-        """build a NTPPacket from a NTP packet received from network
-        raise an NTPException in case of invalid packet format"""
+        """Populate this instance from a NTP packet payload received from
+        the network.
+
+        Parameters:
+        data -- buffer payload
+
+        Raises:
+        NTPException -- in case of invalid packet format
+        """
         try:
-            unpacked = struct.unpack(NTPPacket.ntp_packet_format,
-                    data[0:struct.calcsize(NTPPacket.ntp_packet_format)])
+            unpacked = struct.unpack(NTPPacket._PACKET_FORMAT,
+                    data[0:struct.calcsize(NTPPacket._PACKET_FORMAT)])
         except struct.error:
-            raise NTPException('Invalid NTP packet')
+            raise NTPException("Invalid NTP packet.")
 
         self.leap = unpacked[0] >> 6 & 0x3
         self.version = unpacked[0] >> 3 & 0x7
@@ -107,193 +185,254 @@ class NTPPacket(object):
         self.root_delay = float(unpacked[4])/2**16
         self.root_dispersion = float(unpacked[5])/2**16
         self.ref_id = unpacked[6]
-        self.ref_timestamp = to_time(unpacked[7], unpacked[8])
-        self.orig_timestamp = to_time(unpacked[9], unpacked[10])
-        self.recv_timestamp = to_time(unpacked[11], unpacked[12])
-        self.tx_timestamp = to_time(unpacked[13], unpacked[14])
-        
+        self.ref_timestamp = _to_time(unpacked[7], unpacked[8])
+        self.orig_timestamp = _to_time(unpacked[9], unpacked[10])
+        self.recv_timestamp = _to_time(unpacked[11], unpacked[12])
+        self.tx_timestamp = _to_time(unpacked[13], unpacked[14])
+
 
 class NTPStats(NTPPacket):
-    """wrapper for NTPPacket, offering additional statistics like offset and
-    delay, and timestamps converted to local time"""
+    """NTP statistics.
+
+    Wrapper for NTPPacket, offering additional statistics like offset and delay,
+    and timestamps converted to system time.
+    """
     
-    def __init__(self, dest_timestamp):
+    def __init__(self):
+        """Constructor."""
         NTPPacket.__init__(self)
-        self.dest_timestamp = dest_timestamp
-        
+        self.dest_timestamp = 0
+        """destination timestamp"""
+
     @property
     def offset(self):
-        """NTP offset"""
+        """offset"""
         return ((self.recv_timestamp - self.orig_timestamp) +
-                    (self.tx_timestamp - self.dest_timestamp))/2
-    
+                (self.tx_timestamp - self.dest_timestamp))/2
+
     @property
     def delay(self):
-        """NTP delay"""
+        """round-trip delay"""
         return ((self.dest_timestamp - self.orig_timestamp) -
-                    (self.tx_timestamp - self.recv_timestamp))
-    
+                (self.tx_timestamp - self.recv_timestamp))
+
     @property
     def tx_time(self):
-        """tx_timestamp - local time"""
+        """Transmit timestamp in system time."""
         return ntp_to_system_time(self.tx_timestamp)
 
     @property
     def recv_time(self):
-        """recv_timestamp - local time"""
+        """Receive timestamp in system time."""
         return ntp_to_system_time(self.recv_timestamp)
     
     @property
     def orig_time(self):
-        """orig_timestamp - local time"""
+        """Originate timestamp in system time."""
         return ntp_to_system_time(self.orig_timestamp)
     
     @property
     def ref_time(self):
-        """ref_timestamp - local time"""
+        """Reference timestamp in system time."""
         return ntp_to_system_time(self.ref_timestamp)
     
     @property
     def dest_time(self):
-        """dest_timestamp - local time"""
+        """Destination timestamp in system time."""
         return ntp_to_system_time(self.dest_timestamp)
 
 
-class NTPClient(object):
-    """Client session - for now, a mere wrapper for NTP requests"""
-    
-    def request(self, host, version=2, port='ntp'):
-        """make a NTP request to a server - return a NTPStats object"""
+class NTPClient:
+    """NTP client session."""
+
+    def __init__(self):
+        """Constructor."""
+        pass
+
+    def request(self, host, version=2, port='ntp', timeout=5):
+        """Query a NTP server.
+
+        Parameters:
+        host    -- server name/address
+        version -- NTP version to use
+        port    -- server port
+        timeout -- timeout on socket operations
+
+        Returns:
+        NTPStats object
+        """
         # lookup server address
         addrinfo = socket.getaddrinfo(host, port)[0]
         family, sockaddr = addrinfo[0], addrinfo[4]
 
         # create the socket
         s = socket.socket(family, socket.SOCK_DGRAM)
-        s.settimeout(5)
 
-        # create the request packet - mode 3 is client
-        query = NTPPacket(mode=3, version=version,
-                            tx_timestamp=system_to_ntp_time(time.time()))
-        query_packet = query.to_data()
-    
         try:
+            s.settimeout(timeout)
+
+            # create the request packet - mode 3 is client
+            query_packet = NTPPacket(mode=3, version=version,
+                                  tx_timestamp=system_to_ntp_time(time.time()))
+
             # send the request
-            s.sendto(query_packet, sockaddr)
+            s.sendto(query_packet.to_data(), sockaddr)
             
             # wait for the response - check the source address
-            src_addr = (None, None)
+            src_addr = None,
             while src_addr[0] != sockaddr[0]:
-                (response_packet, src_addr) = s.recvfrom(256)
+                response_packet, src_addr = s.recvfrom(256)
 
             # build the destination timestamp
             dest_timestamp = system_to_ntp_time(time.time())
+        except socket.timeout:
+            raise NTPException("No response received from %s." % host)
         finally:
-            # no matter what happens, we must close the socket
-            # if an exception was raised, let the application hanle it
             s.close()
                     
         # construct corresponding statistics
-        response = NTPStats(dest_timestamp)
-        response.from_data(response_packet)
+        stats = NTPStats()
+        stats.from_data(response_packet)
+        stats.dest_timestamp = dest_timestamp
         
-        return response
+        return stats 
     
 
-def to_int(date):
-    """return the integral part of a timestamp"""
-    return int(date)
+def _to_int(timestamp):
+    """Return the integral part of a timestamp.
 
-def to_frac(date, n=32):
-    """return the fractional part of a timestamp - n is the number of bits of
-    the fractional part"""
-    return int(abs(date - to_int(date)) * 2**n)
+    Parameters:
+    timestamp -- NTP timestamp
 
-def to_time(integ, frac, n=32):
-    """build a timestamp from an integral and fractional part - n is the
-    number of bits of the fractional part"""
+    Retuns:
+    integral part
+    """
+    return int(timestamp)
+
+def _to_frac(timestamp, n=32):
+    """Return the fractional part of a timestamp.
+
+    Parameters:
+    timestamp -- NTP timestamp
+    n         -- number of bits of the fractional part
+
+    Retuns:
+    fractional part
+    """
+    return int(abs(timestamp - _to_int(timestamp)) * 2**n)
+
+def _to_time(integ, frac, n=32):
+    """Return a timestamp from an integral and fractional part.
+
+    Parameters:
+    integ -- integral part
+    frac  -- fractional part
+    n     -- number of bits of the fractional part
+
+    Retuns:
+    timestamp
+    """
     return integ + float(frac)/2**n
 
-def ntp_to_system_time(date):
-    """convert a NTP time to system time"""
-    return date - NTP_DELTA
+def ntp_to_system_time(timestamp):
+    """Convert a NTP time to system time.
 
-def system_to_ntp_time(date):
-    """convert a system time to a NTP time"""
-    return date + NTP_DELTA
+    Parameters:
+    timestamp -- timestamp in NTP time
+
+    Returns:
+    corresponding system time
+    """
+    return timestamp - NTP.NTP_DELTA
+
+def system_to_ntp_time(timestamp):
+    """Convert a system time to a NTP time.
+
+    Parameters:
+    timestamp -- timestamp in system time
+
+    Returns:
+    corresponding NTP time
+    """
+    return timestamp + NTP.NTP_DELTA
 
 def leap_to_text(leap):
-    """convert a leap value to text"""
-    leap_table = {
-        0: 'no warning',
-        1: 'last minute has 61 seconds',
-        2: 'last minute has 59 seconds',
-        3: 'alarm condition (clock not synchronized)',
-    }
+    """Convert a leap indicator to text.
 
-    if leap in leap_table:
-        return leap_table[leap]
+    Parameters:
+    leap -- leap indicator value
+
+    Returns:
+    corresponding message
+
+    Raises:
+    NTPException -- in case of invalid leap indicator
+    """
+    if leap in NTP.LEAP_TABLE:
+        return NTP.LEAP_TABLE[leap]
     else:
-        raise NTPException('Invalid leap indicator')
+        raise NTPException("Invalid leap indicator.")
 
 def mode_to_text(mode):
-    """convert a mode value to text"""
-    mode_table = {
-        0: 'unspecified',
-        1: 'symmetric active',
-        2: 'symmetric passive',
-        3: 'client',
-        4: 'server',
-        5: 'broadcast',
-        6: 'reserved for NTP control messages',
-        7: 'reserved for private use',
-    }
+    """Convert a NTP mode value to text.
 
-    if mode in mode_table:
-        return mode_table[mode]
+    Parameters:
+    mode -- NTP mode
+
+    Returns:
+    corresponding message
+
+    Raises:
+    NTPException -- in case of invalid mode
+    """
+    if mode in NTP.MODE_TABLE:
+        return NTP.MODE_TABLE[mode]
     else:
-        raise NTPException('Invalid mode')
+        raise NTPException("Invalid mode.")
 
 def stratum_to_text(stratum):
-    """convert a stratum value to text"""
-    stratum_table = {
-        0: 'unspecified',
-        1: 'primary reference',
-    }
+    """Convert a stratum value to text.
 
-    if stratum in stratum_table:
-        return stratum_table[stratum]
+    Parameters:
+    stratum -- NTP stratum
+
+    Returns:
+    corresponding message
+
+    Raises:
+    NTPException -- in case of invalid stratum
+    """
+    if stratum in NTP.STRATUM_TABLE:
+        return NTP.STRATUM_TABLE[stratum]
     elif 1 < stratum < 255:
-        return 'secondary reference (NTP)'
+        return "secondary reference (NTP)"
     else:
-        raise NTPException('Invalid stratum')
+        raise NTPException("Invalid stratum.")
 
 def ref_id_to_text(ref_id, stratum=2):
-    """convert a reference identifier to text according to stratum"""
-    ref_id_table = {
-            'DNC': 'DNC routing protocol',
-            'NIST': 'NIST public modem',
-            'TSP': 'TSP time protocol',
-            'DTS': 'Digital Time Service',
-            'ATOM': 'Atomic clock (calibrated)',
-            'VLF': 'VLF radio (OMEGA, etc)',
-            'callsign': 'Generic radio',
-            'LORC': 'LORAN-C radionavidation',
-            'GOES': 'GOES UHF environment satellite',
-            'GPS': 'GPS UHF satellite positioning',
-    }
+    """Convert a reference clock identifier to text according to its stratum.
 
+    Parameters:
+    ref_id  -- reference clock indentifier
+    stratum -- NTP stratum
+
+    Returns:
+    corresponding message
+
+    Raises:
+    NTPException -- in case of invalid stratum
+    """
     fields = (ref_id >> 24 & 0xff, ref_id >> 16 & 0xff,
                 ref_id >> 8 & 0xff, ref_id & 0xff)
 
     # return the result as a string or dot-formatted IP address
     if 0 <= stratum <= 1 :
         text = '%c%c%c%c' % fields
-        if text in ref_id_table:
-            return ref_id_table[text]
+        if text in NTP.REF_ID_TABLE:
+            return NTP.REF_ID_TABLE[text]
         else:
             return text
     elif 2 <= stratum < 255:
         return '%d.%d.%d.%d' % fields
     else:
-        raise NTPException('Invalid reference clock identifier')
+        raise NTPException("Invalid stratum.")
--- test_ntplib.py
+++ test_ntplib.py
@@ -1,39 +1,102 @@
-#! /usr/bin/env python
-"""ntplib test script.
-Send a request to a NTP server, and analyse the response.
+"""Python NTP library tests.
 """
 
-import sys
-from time import ctime
+
+import socket
+import time
+import unittest
 
 import ntplib
 
 
-if len(sys.argv) > 1:
-    hostname = sys.argv[1]
-else:
-    sys.stdout.write('NTP server ? ')
-    hostname = sys.stdin.readline().strip()
+class TestNTPLib(unittest.TestCase):
 
-client = ntplib.NTPClient()
-response = client.request(hostname, version=3)
+    NTP_SERVER = "pool.ntp.org"
+    """test NTP server"""
 
-print('Version number : %d' % response.version)
-print('Offset : %f' % response.offset)
-print('Stratum : %s (%d)' % (ntplib.stratum_to_text(response.stratum),
-    response.stratum))
-print('Precision : %d' % response.precision)
-print('Root delay : %f ' % response.root_delay)
-print('Root dispersion : %f' % response.root_dispersion)
-print('Delay : %f' % response.delay)
-print('Leap indicator : %s (%d)' % (ntplib.leap_to_text(response.leap),
-    response.leap))
-print('Poll : %d' % response.poll)
-print('Mode : %s (%d)' % (ntplib.mode_to_text(response.mode), response.mode))
-print('Transmit timestamp : ' + ctime(response.tx_time))
-print('Reference timestamp : ' + ctime(response.ref_time))
-print('Original timestamp : ' + ctime(response.orig_time))
-print('Receive timestamp : ' + ctime(response.recv_time))
-print('Destination timestamp : ' + ctime(response.dest_time))
-print('Reference clock identifier : ' + ntplib.ref_id_to_text(response.ref_id,
-    response.stratum))
+    POLL_DELAY = 1
+    """delay between NTP polls, in seconds"""
+
+    DELTA_TOLERANCE = 0.5
+    """tolerance for offset/delay comparisons, in seconds"""
+
+
+    def test_basic(self):
+        """Basic tests."""
+        self.assertTrue(issubclass(ntplib.NTPException, Exception))
+
+        client = ntplib.NTPClient()
+
+    def test_request(self):
+        """Request tests."""
+        client = ntplib.NTPClient()
+
+        t1 = time.time()
+        info = client.request(self.NTP_SERVER)
+        t2 = time.time()
+
+        # check response
+        self.assertTrue(isinstance(info, ntplib.NTPStats))
+        self.assertTrue(0 < info.version <= 4)
+        self.assertTrue(isinstance(info.offset, float))
+        self.assertTrue(0 <= info.stratum < 0xff)
+        self.assertTrue(-0x7f <= info.precision < 0x7f)
+        self.assertTrue(isinstance(info.root_delay, float))
+        self.assertTrue(isinstance(info.root_dispersion, float))
+        self.assertTrue(isinstance(info.delay, float))
+        self.assertTrue(isinstance(info.leap, int))
+        self.assertIn(info.leap, ntplib.NTP.LEAP_TABLE)
+        self.assertTrue(0 <= info.poll < 0xfff)
+        self.assertTrue(isinstance(info.mode, int))
+        self.assertIn(info.mode, ntplib.NTP.MODE_TABLE)
+        self.assertTrue(0 <= info.ref_id < 0xffffffff)
+        self.assertTrue(isinstance(info.tx_time, float))
+        self.assertTrue(isinstance(info.ref_time, float))
+        self.assertTrue(isinstance(info.orig_time, float))
+        self.assertTrue(isinstance(info.recv_time, float))
+        self.assertTrue(isinstance(info.dest_time, float))
+
+        time.sleep(self.POLL_DELAY)
+
+        new_info = client.request(self.NTP_SERVER)
+
+        # check timestamps
+        self.assertTrue(t1 < info.orig_time < info.dest_time < t2)
+        self.assertTrue(info.orig_time < new_info.orig_time)
+        self.assertTrue(info.recv_time < new_info.recv_time)
+        self.assertTrue(info.tx_time < new_info.tx_time)
+        self.assertTrue(info.dest_time < new_info.dest_time)
+
+        # check the offset and delay
+        self.assertLess(abs(new_info.offset - info.offset), self.DELTA_TOLERANCE)
+        self.assertLess(abs(new_info.delay - info.delay), self.DELTA_TOLERANCE)
+
+        # try reaching a non existent server
+        self.assertRaises(ntplib.NTPException,
+                          client.request, "localhost", port=42)
+
+        # try reaching a non existent server with a custom timeout
+        t = time.time()
+        self.assertRaises(ntplib.NTPException,
+                          client.request, "localhost", port=42, timeout=1)
+        self.assertTrue(0.7 < time.time() - t < 1.3)
+
+    def test_helpers(self):
+        """Helper methods tests."""
+        client = ntplib.NTPClient()
+
+        time.sleep(self.POLL_DELAY)
+        info = client.request(self.NTP_SERVER)
+
+        self.assertEqual(int(info.tx_time), ntplib.ntp_to_system_time(
+                            ntplib.system_to_ntp_time(int(info.tx_time))))
+
+        self.assertTrue(isinstance(ntplib.leap_to_text(info.leap), str))
+        self.assertTrue(isinstance(ntplib.mode_to_text(info.mode), str))
+        self.assertTrue(isinstance(ntplib.stratum_to_text(info.stratum), str))
+        self.assertTrue(isinstance(ntplib.ref_id_to_text(info.ref_id,
+                                                         info.stratum), str))
+
+
+if __name__ == '__main__':
+    unittest.main()
